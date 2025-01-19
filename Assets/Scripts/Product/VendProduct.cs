1a08490229aa721c33f3130f7915bd65cf9eca52
rickychak <rickychak200012@gmail.com>
2025-01-20 02:11:16 +0800

    Set up UI for interactable kitchen items
    1. will need to optimise by creating abstract base class as all interactable items have the same UI functionality, too much duplication

---
+++ Assets/Scripts/Product/VendProduct.cs
@@ -0,0 +1,56 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Komorebi.Product
+{
+    public class VendProduct:  MonoBehaviour, IInteractable
+    {
+        private bool _isActivated = false;
+        private bool _isClosed = true;
+       
+        
+        private SpriteRenderer _spriteRenderer;
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            _animator = GetComponentInChildren<Animator>();
+            
+        }
+
+        private void Start()
+        {
+            // _animator.SetBool(IsClosed, _isClosed);
+        }
+
+        public bool IsActivated()
+        {
+            return _isActivated;
+        }
+
+        public void Toggle()
+        {
+            
+        }
+
+        public void ShowPrompt()
+        {
+            _ = _isActivated ? _spriteRenderer.DOFade(0, 0.5f) : _spriteRenderer.DOFade(1, 0.5f);
+            _isActivated = !_isActivated;
+            
+        }
+
+        public void Interact()
+        {
+            // _isClosed = !_isClosed;
+            // _animator.SetBool(IsClosed, _isClosed);
+            // _animator.SetTrigger(AnimToggle);
+        }
+
+        public GameObject GetGameObject()
+        {
+            return this.gameObject;
+        }
+    }
+}
